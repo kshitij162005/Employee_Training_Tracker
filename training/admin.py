f25f399b20a009658baf46923dd9b6a30c57bd9e
Kshitij Kotecha <kotechakshitij@gmail.com>
2025-04-16 11:03:04 +0530

    Setted up  Django Admin and performed DB migrations.

--- training/admin.py
+++ training/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
+from .models import Employee, TrainingProgram, Enrollment
 
 # Register your models here.
+
+# Employee Admin Registration
+@admin.register(Employee)
+class EmployeeAdmin(admin.ModelAdmin):
+    list_display = ('name', 'email', 'department', 'designation')  
+    search_fields = ('name', 'email', 'department', 'designation')
+
+# Training Program Admin Registration
+@admin.register(TrainingProgram)
+class TrainingProgramAdmin(admin.ModelAdmin):
+    list_display = ('title', 'start_date', 'end_date', 'trainer_name')  
+    search_fields = ('title', 'trainer_name')
+
+# Enrollment Admin Registration
+@admin.register(Enrollment)
+class EnrollmentAdmin(admin.ModelAdmin):
+    list_display = ('employee', 'training_program', 'status')  
+    list_filter = ('status',)  
